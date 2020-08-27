b0ca8916809f8201179486e5c7416a4d28f60293
Anikeat Sharma <anikeat1202@gmail.com>
2020-08-27 13:04:42 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Notes from "./Notes";
+
+var App=function(){
+
+
+return <div><Header></Header> 
+
+<Notes></Notes>
+<Footer></Footer>
+      
+</div>
+
+}
+
+
+export default App;
