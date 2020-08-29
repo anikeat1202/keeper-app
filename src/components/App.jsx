dee3e7a1becf92ba3e97be629cfae20c6c978d58
Anikeat Sharma <anikeat1202@gmail.com>
2020-08-29 14:31:59 +0530

    Add files via upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Notes from "./Notes";
+import notes from "../notes"
+
+var noteEntry = (note)=>{
+
+return <Notes key={note.id} title={note.title} content={note.content}></Notes>
+
+}
 
 var App=function(){
 
 
 return <div><Header></Header> 
-
-<Notes></Notes>
+{notes.map(noteEntry)}
 <Footer></Footer>
-      
 </div>
 
 }
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import notes from "../notes"
 
-var Notes=function(){
+
+
+var Notes=function(props){
 
 return  <div className="note">
-<h1>This is the note title</h1>
- <p>This is the note content</p>
+<h1>{props.title}</h1>
+ <p>{props.content}</p>
  </div>
 
 }
